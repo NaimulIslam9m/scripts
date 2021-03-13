e21ea718170361d78e0e77f471cb68c2d197cd94
Naim <naim2018331019@gmail.com>
2021-03-13 23:20:23 +0600

    updated

--- up.sh
+++ up.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-x=1
+printf "ENTER THE 'DOMAIN NAME' YOU WANT TO CHECK: "
+read input
 
-while :; do
-    if [ `ping -c 1 $1 | grep packet | awk '{print $4}'` -eq 1 ]; then
-        say 'sir, you are connected'
-        break
-    else
-        echo $x
-        ((x+=1))
-    fi
-done
+
+if [[ `dig $input | grep ANSWER | wc -l` -eq 2 ]]; then
+
+    x=1
+    while :; do
+        if [[ `ping -c 1 $input | grep packet | awk '{print $4}'` -eq 1 ]]; then
+            say 'sir, you are connected'
+            break
+        else
+            echo $x
+            ((x+=1))
+        fi
+    done
+
+else
+    echo "INVALID DOMAIN NAME"
+fi

f7d7125969a7257e97f9e22abcc48269bb131f5f
Naim <naim2018331019@gmail.com>
2021-03-15 14:08:24 +0600

    updated

--- up.sh
+++ up.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-printf "ENTER THE 'DOMAIN NAME' YOU WANT TO CHECK: "
-read input
+if [[ ${#} != 1 ]]; then
+    echo "ERROR: YOU FORGOT TO ENTER THE 'DOMAIN NAME' (e.g. up.sh github.com)"
+    exit
+fi
 
 
-if [[ `dig $input | grep ANSWER | wc -l` -eq 2 ]]; then
+if [[ `dig ${1} | grep ANSWER | wc -l` -eq 2 ]]; then
 
     x=1
     while :; do
-        if [[ `ping -c 1 $input | grep packet | awk '{print $4}'` -eq 1 ]]; then
+        if [[ `ping -c 1 ${1} | grep packet | awk '{print $4}'` -eq 1 ]]; then
             say 'sir, you are connected'
             break
         else

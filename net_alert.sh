104515b1ff895b1156a89062109174ace1949c9d
Naim <naim2018331019@gmail.com>
2021-03-15 13:39:44 +0600

    updated

--- net_alert.sh
+++ net_alert.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 discon=0
+up=1
 
 while :; do
     if [[ `ping -c 1 8.8.8.8 | grep packet | awk '{print $4}'` -eq 1 ]]; then
         if [[ discon -eq 1 ]] ; then
+            say "sir, you are connected"
             echo ""
             discon=0
         fi
-        echo ✅
-        say 'sir, you are connected'
+        echo ✅ $up
+        ((up+=1))
         # break
     else
         printf ❎
         discon=1
+        up=1
     fi
 done

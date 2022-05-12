09b7141c64ecae456cd52985988512c34b9d7f17
Naimul Islam <naimul9m@gmail.com>
2022-05-12 23:15:43 +0600

    Update set_key.sh

--- set_key.sh
+++ set_key.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
-echo "usage: ./set_key.sh <key> <repo name>"
-username='NaimulIslam9m'
-git remote set-url origin https://$username:$1@github.com/$username/$2
+if [ $# -eq 2 ]; then
+    username='NaimulIslam9m'
+    git remote set-url origin https://$username:$1@github.com/$username/$2
+    git remote -v
+    echo 'OK'
+else
+    echo "usage: ./set_key.sh <key> <repo name>"
+fi
